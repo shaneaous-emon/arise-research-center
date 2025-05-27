1fe8c445d28edce3fead1365b08472d6a3934aba
Md. Shaneaous Emon <mdshaneaous510@gmail.com>
2025-05-27 15:01:41 +0600

    last update 3

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "https://shaneaous-emon.github.io/arise-research-center",
+  base: "/arise-research-center/",
   plugins: [react()],
   resolve: {
     alias: {
