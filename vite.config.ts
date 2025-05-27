43431571075b96375fb4fd21bdd269c19e550b9b
Md. Shaneaous Emon <61546066+shaneaous-emon@users.noreply.github.com>
2025-05-27 15:12:17 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/arise-research-center/",
   plugins: [react()],
   resolve: {
     alias: {
